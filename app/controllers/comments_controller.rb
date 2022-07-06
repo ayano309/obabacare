2953805d576b79043c4d468be0a3af792c7d7349
ayano309 <ayatomato.k@gmail.com>
2022-07-06 15:43:32 +0000

    [update]commentの非同期化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,14 +5,14 @@ class CommentsController < ApplicationController
     @vital = Vital.find(params[:vital_id])
     @comment = @vital.comments.new(comment_params)
     @comment.save
-    redirect_to vital_path(@vital), notice: 'コメントを追加'
+    
   end
 
   def destroy
     @vital = Vital.find(params[:vital_id])
     @comment = @vital.comments.find(params[:id])
     @comment.destroy
-    redirect_to vital_path(@vital), notice: 'コメントを削除'
+    
   end
 
   private
