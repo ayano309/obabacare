e5cb5c047d09bb55d4a49ded21d63f58cadb969a
ayano309 <ayatomato.k@gmail.com>
2022-07-06 15:28:53 +0000

    [add]fecesボタンを非同期化

--- app/controllers/defecations_controller.rb
+++ app/controllers/defecations_controller.rb
@@ -2,16 +2,16 @@ class DefecationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    vital = Vital.find(params[:vital_id])
-    vital.defecations.create!(user_id: current_user.id)
-    redirect_to vital_path(vital)
+    @vital = Vital.find(params[:vital_id])
+    @vital.defecations.create!(user_id: current_user.id)
+    
   end
 
   def destroy
-    vital = Vital.find(params[:vital_id])
-    defecation = vital.defecations.find_by!(user_id: current_user.id)
+    @vital = Vital.find(params[:vital_id])
+    defecation = @vital.defecations.find_by!(user_id: current_user.id)
 
     defecation.destroy!
-    redirect_to vital_path(vital)
+    
   end
 end
