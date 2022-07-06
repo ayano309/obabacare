9e5228c8db41c15158a7122ba399de6b688e2a16
ayano309 <ayatomato.k@gmail.com>
2022-07-06 10:13:22 +0000

    [add]日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module ObabaCare
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators.system_tests = nil
-
+    config.time_zone = 'Asia/Tokyo'
     config.generators do |g|
       g.skip_routes true
       g.assets false
