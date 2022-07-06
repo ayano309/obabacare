fa38579f73a649e4eff5beb963fe1d9a4fa08582
ayano309 <ayatomato.k@gmail.com>
2022-07-06 10:14:05 +0000

    [add]日本語化

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module ObabaCare
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators.system_tests = nil
     config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
     config.generators do |g|
       g.skip_routes true
       g.assets false
