befb9926e91bf393f05b43daa1c3cd9bd27302c1
ayano309 <ayatomato.k@gmail.com>
2022-07-06 14:16:55 +0000

    [add]defecationsモデルのアソシエーション

--- db/migrate/20220706141428_create_defecations.rb
+++ db/migrate/20220706141428_create_defecations.rb
@@ -1,7 +1,8 @@
 class CreateDefecations < ActiveRecord::Migration[6.1]
   def change
     create_table :defecations do |t|
-
+      t.references :user, null: false
+      t.references :vital, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_120440) do
+ActiveRecord::Schema.define(version: 2022_07_06_141428) do
+
+  create_table "defecations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "vital_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_defecations_on_user_id"
+    t.index ["vital_id"], name: "index_defecations_on_vital_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
