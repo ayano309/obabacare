8b6489c761b0eb2e15f466b1e7ad6d63680b53eb
ayano309 <ayatomato.k@gmail.com>
2022-07-07 15:15:47 +0000

    [add]memoコントローラーアクション

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,10 +1,53 @@
 class MemosController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_memo, only: %i[edit update]
+  before_action :set_user, only: %i[index new create destroy]
+  
   def index
+    @memos = Vital.on_memos(@user, params[:page])
   end
 
   def new
+    @memo = @user.memos.build
   end
 
-  def edit
+  def create
+    @memo = @user.memos.build(memo_params)
+    if @memo.save
+      redirect_to memos_path, notice: '保存できました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @memo.update(memo_params)
+      redirect_to memos_path, notice: '更新できました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    memo = @user.memos.find(params[:id])
+    memo.destroy!
+    redirect_to memos_path, notice: '削除に成功しました'
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:content,:title)
+  end
+
+  def set_memo
+    @memo = current_user.memos.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
   end
 end
--- app/models/memo.rb
+++ app/models/memo.rb
@@ -3,6 +3,8 @@
 # Table name: memos
 #
 #  id         :integer          not null, primary key
+#  content    :text             not null
+#  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :integer          not null
@@ -12,4 +14,9 @@
 #  index_memos_on_user_id  (user_id)
 #
 class Memo < ApplicationRecord
+  scope :memo_by_user, -> (user) { where(user_id: user) }
+  scope :on_memos, -> (user,page) {
+    vitals_by_user(user).
+    display_list(page)
+  }
 end
