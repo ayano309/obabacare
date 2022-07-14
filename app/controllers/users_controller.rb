ee45a6fece6136c1d48840b4cf5a20bafb0a93dd
ayano309 <ayatomato.k@gmail.com>
2022-07-14 01:54:53 +0000

    [create]usersコントローラー

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+   before_action :authenticate_user!
+   
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  
+end
