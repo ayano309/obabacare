2487c5cfaf4ae50b6b6fa20cbd235fb944c7e6e5
ayano309 <ayatomato.k@gmail.com>
2022-07-15 10:21:49 +0000

    [fix]bulletより修正

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,5 +1,5 @@
 class EmotionsController < ApplicationController
   def index
-    @comments = Comment.joins(:vital).by_user(current_user)
+    @comments = Comment.eager_load(:vital).by_user(current_user)
   end
 end
