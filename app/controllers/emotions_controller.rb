db9495cdc09ee77b120fbbfc22e155be71ed36f9
ayano309 <ayatomato.k@gmail.com>
2022-07-16 04:27:50 +0000

    [add]コメント追加

--- app/controllers/emotions_controller.rb
+++ app/controllers/emotions_controller.rb
@@ -1,6 +1,7 @@
 class EmotionsController < ApplicationController
   before_action :authenticate_user!
   def index
+    #コメントのemotionカラムを表示したい
     @comments = Comment.eager_load(:vital).by_user(current_user)
   end
 end
