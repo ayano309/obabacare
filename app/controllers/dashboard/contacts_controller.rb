924e87715d1445c334388cddfed53c3fe18943a9
ayano309 <ayatomato.k@gmail.com>
2022-07-17 06:11:18 +0000

    [add]管理者画面index,show画面

--- app/controllers/dashboard/contacts_controller.rb
+++ app/controllers/dashboard/contacts_controller.rb
@@ -1,10 +1,11 @@
 class Dashboard::ContactsController < ApplicationController
   before_action :authenticate_admin!
   layout 'dashboard/dashboard'
-  
   def index
+    @contacts = Contact.all.order(created_at: 'desc')
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 end
