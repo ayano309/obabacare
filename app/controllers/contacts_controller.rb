556601a505e720079bc600ba313f9f0365985303
ayano309 <ayatomato.k@gmail.com>
2022-07-17 05:32:30 +0000

    [add]お問い合わせ機能

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,21 @@
 class ContactsController < ApplicationController
   def index
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to root_path, notice: 'お問い合わせを送信しました。'
+    else
+      flash.now[:error] = 'お問い合わせの送信に失敗しました。'
+      render :index
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :phone, :message)
   end
 end
