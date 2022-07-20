cdfc75c2ae52bbc2cae83583799ef766a246d4d2
ayano309 <ayatomato.k@gmail.com>
2022-07-20 13:56:39 +0000

    [add]Vitalのモデルテスト(各モデルとのアソシエーション)

--- spec/models/vital_spec.rb
+++ spec/models/vital_spec.rb
@@ -54,5 +54,53 @@ RSpec.describe Vital, type: :model do
       vital.valid?
       expect(vital.errors.of_kind?(:oxygen_saturation, :blank)).to be_truthy
     end
+    
+    describe '各モデルとのアソシエーション' do
+      before do
+        @vital = FactoryBot.create(:vital)
+        user = FactoryBot.create(:user)
+        Defecation.create(user_id: user.id, vital_id: @vital.id)
+        Comment.create(user_id: user.id, vital_id: @vital.id, content: 'comment', emotion: 0, is_important: false)
+        sleep 0.1
+      end
+  
+      let(:association) do
+        described_class.reflect_on_association(target)
+      end
+  
+      context 'Userモデルとのアソシエーション' do
+        let(:target) { :user }
+  
+        it 'Userとの関連付けはbelongs_toであること' do
+          expect(association.macro).to eq :belongs_to
+        end
+      end
+  
+  
+      context "Defecationモデルとのアソシエーション" do
+        let(:target) { :defecations }
+  
+        it "Defecationとの関連付けはhas_manyであること" do
+          expect(association.macro).to eq :has_many
+        end
+  
+        it "Vitalが削除されたらDefecationも削除されること" do
+          expect { @vital.destroy }.to change(Defecation, :count).by(-1)
+        end
+      end
+  
+      context 'Commentモデルとのアソシエーション' do
+        let(:target) { :comments }
+        it 'Commentとの関連付けはhas_manyであること' do
+          expect(association.macro).to eq :has_many
+        end
+  
+        it 'Vitalが削除されたらcommentsも削除されること' do
+          expect { @vital.destroy }.to change(Comment, :count).by(-1)
+        end
+      end
+  
+      
+    end
  end
 end
