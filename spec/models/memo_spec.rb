70582e79fd5c5faaae7154f7d21260ca1a1c71ad
ayano309 <ayatomato.k@gmail.com>
2022-07-20 14:33:40 +0000

    [add]memoのmodelテスト(各モデルとのアソシエーション)

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -25,4 +25,18 @@ RSpec.describe Memo, type: :model do
       expect(memo.errors.of_kind?(:category, :blank)).to be_truthy
     end
   end
+  
+  describe "各モデルとのアソシエーション" do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context "Userモデルとのアソシエーション" do
+      let(:target) { :user }
+
+      it "Userとの関連付けはbelongs_toであること" do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+  end
 end
