61b90f9a1bb50974be667ed4f72ef8e49a37c736
ayano309 <ayatomato.k@gmail.com>
2022-07-20 13:46:44 +0000

    [add]vitalのmodelテスト

--- spec/models/vital_spec.rb
+++ spec/models/vital_spec.rb
@@ -6,5 +6,53 @@ RSpec.describe Vital, type: :model do
       vital = build(:vital)
       expect(vital).to be_valid
     end
-  end
+  
+    it 'dayが存在しないと無効' do
+      vital = FactoryBot.build(:vital, day: '')
+      vital.valid?
+      expect(vital.errors.of_kind?(:day, :blank)).to be_truthy
+    end
+    
+    it 'high_bpが存在しないと無効' do
+      vital = FactoryBot.build(:vital, high_bp: '')
+      vital.valid?
+      expect(vital.errors.of_kind?(:high_bp, :blank)).to be_truthy
+    end
+    
+    it 'low_bpが存在しないと無効' do
+      vital = FactoryBot.build(:vital, low_bp: '')
+      vital.valid?
+      expect(vital.errors.of_kind?(:low_bp, :blank)).to be_truthy
+    end
+    
+    it 'breathingが存在しないと無効' do
+      vital = FactoryBot.build(:vital, breathing: '')
+      vital.valid?
+      expect(vital.errors.of_kind?(:breathing, :blank)).to be_truthy
+    end
+    
+    it 'pulseが存在しないと無効' do
+      vital = FactoryBot.build(:vital,  pulse: '')
+      vital.valid?
+      expect(vital.errors.of_kind?(:pulse, :blank)).to be_truthy
+    end
+    
+    it 'temperatureが存在しないと無効' do
+      vital = FactoryBot.build(:vital,  temperature: '')
+      vital.valid?
+      expect(vital.errors.of_kind?(:temperature, :blank)).to be_truthy
+    end
+    
+    it 'moisture_supplyが存在しないと無効' do
+      vital = FactoryBot.build(:vital,  moisture_supply: '')
+      vital.valid?
+      expect(vital.errors.of_kind?(:moisture_supply, :blank)).to be_truthy
+    end
+    
+    it 'oxygen_saturationが存在しないと無効' do
+      vital = FactoryBot.build(:vital,  oxygen_saturation: '')
+      vital.valid?
+      expect(vital.errors.of_kind?(:oxygen_saturation, :blank)).to be_truthy
+    end
+ end
 end
