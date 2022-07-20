f800d0bee0af9e6881d0b226c0756e633cab89fd
ayano309 <ayatomato.k@gmail.com>
2022-07-20 14:29:36 +0000

    [add]memoのmodelテスト

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -6,5 +6,23 @@ RSpec.describe Memo, type: :model do
       memo = build(:memo)
       expect(memo).to be_valid
     end
+    
+    it 'titleが存在しない場合無効であること' do
+      memo = FactoryBot.build(:memo, title: '')
+      memo.valid?
+      expect(memo.errors.of_kind?(:title, :blank)).to be_truthy
+    end
+    
+    it 'contentが存在しない場合無効であること' do
+      memo = FactoryBot.build(:memo, content: '')
+      memo.valid?
+      expect(memo.errors.of_kind?(:content, :blank)).to be_truthy
+    end
+    
+    it 'categoryが存在しない場合無効であること' do
+      memo = FactoryBot.build(:memo, category: '')
+      memo.valid?
+      expect(memo.errors.of_kind?(:category, :blank)).to be_truthy
+    end
   end
 end
