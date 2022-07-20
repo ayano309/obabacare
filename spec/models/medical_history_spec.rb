2758013808e3fea4aaf062ff3ea4f67144a95c83
ayano309 <ayatomato.k@gmail.com>
2022-07-20 14:42:19 +0000

    [add]medical_historyのmodelテスト(各モデルとのアソシエーション)

--- spec/models/medical_history_spec.rb
+++ spec/models/medical_history_spec.rb
@@ -6,5 +6,32 @@ RSpec.describe MedicalHistory, type: :model do
       medical_history = build(:medical_history)
       expect(medical_history).to be_valid
     end
+    
+    it 'disease_nameが存在しない場合無効であること' do
+      medical_history = FactoryBot.build(:medical_history, disease_name: '')
+      medical_history.valid?
+      expect(medical_history.errors.of_kind?(:disease_name, :blank)).to be_truthy
+    end
+    
+    
+    it 'when_onsetが存在しない場合無効であること' do
+      medical_history = FactoryBot.build(:medical_history, when_onset: '')
+      medical_history.valid?
+      expect(medical_history.errors.of_kind?(:when_onset, :blank)).to be_truthy
+    end
+  end
+  
+  describe "各モデルとのアソシエーション" do
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+
+    context "Userモデルとのアソシエーション" do
+      let(:target) { :user }
+
+      it "Userとの関連付けはbelongs_toであること" do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
   end
 end
