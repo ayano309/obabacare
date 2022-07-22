74a049a7ec2e628520988dbee82f603519f30679
ayano309 <ayatomato.k@gmail.com>
2022-07-22 07:12:58 +0000

    [add]重要コメントとの区別つけられるhelper追加

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -2,4 +2,8 @@ module CommentHelper
   def comment_exists?(comments)
     'card_comment_detail' if comments.exists?
   end
+  
+  def important_comment?(comment)
+    'important_comment' if comment.is_important == true
+  end
 end
