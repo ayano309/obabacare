117c4607432b4442fe2a22199a63e7982c96695d
ayano309 <ayatomato.k@gmail.com>
2022-07-24 16:25:26 +0000

    [add]annotateによる修正

--- spec/factories/defecations.rb
+++ spec/factories/defecations.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: defecations
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#  vital_id   :integer          not null
+#
+# Indexes
+#
+#  index_defecations_on_user_id   (user_id)
+#  index_defecations_on_vital_id  (vital_id)
+#
 FactoryBot.define do
   factory :defecation do
     association :user
--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id             :integer          not null, primary key
+#  birthday       :date
+#  blood_type     :integer
+#  gender         :integer
+#  important_term :text
+#  weight         :float
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  user_id        :integer          not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
 FactoryBot.define do
   factory :profile do
     weight { 46.2 }
