c6a29bc68aec887a42983a2ed9f67c887048dfe9
ayano309 <ayatomato.k@gmail.com>
2022-07-24 05:47:57 +0000

    [fix]重要コメント一覧リンクを動的に変更

--- app/helpers/comment_helper.rb
+++ app/helpers/comment_helper.rb
@@ -9,6 +9,13 @@ module CommentHelper
     'important_comment' if comment.is_important == true
   end
   
+  
+  #重要コメントページだったらvital一覧リンク,それ以外は重要コメントリンク
+  def only_this_page(path)
+    path = path.split('?').first  
+    true if current_page?(path)
+  end
+  
   #便秘気味か
   def defecations_exists?(defecations)
     if defecations.count < 2
